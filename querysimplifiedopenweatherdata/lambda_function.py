14363e8658eb602b9c2e7ff618f63dbd4c498b9a
ameyvedpathak <ameyv23@gmail.com>
2020-09-22 18:34:13 +0100

    edited 6

--- querysimplifiedopenweatherdata/lambda_function.py
+++ querysimplifiedopenweatherdata/lambda_function.py
@@ -2,12 +2,11 @@ import json
 import boto3
 
 def lambda_handler(event, context):
+
     dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
     table = dynamodb.Table('simplifiedopenweatherdata')
-    result=[]
-    for i in range(len(data)):
-        resp=(table.get_item(Key={"name": data[i]['name']}))
-        result.append(resp['Item'])
+    response = table.scan(Select="ALL_ATTRIBUTES")
+    result=response['Items']
 
 
 
